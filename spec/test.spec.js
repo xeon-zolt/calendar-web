1afd14868ba973b832e1ebd329d4c3248d4f99b4
Marielle Pointon <marielle@widged.com>
2019-01-31 11:03:52 +0100

    test template

--- spec/test.spec.js
+++
@@ -1,8 +0,0 @@
-console.log("TEST");
-describe("index test", () => {
-  describe("sayHello function", () => {
-    it("should say Hello guys!", () => {
-      console.log("hello");
-    });
-  });
-});
