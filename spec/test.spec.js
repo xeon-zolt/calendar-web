b30a1ae1f1904a84a953040d0401c41434855f3a
Marielle Pointon <marielle@widged.com>
2019-01-31 10:59:58 +0100

    test model logic with 'yarn testflow'

---
+++ spec/test.spec.js
@@ -0,0 +1,8 @@
+console.log("TEST");
+describe("index test", () => {
+  describe("sayHello function", () => {
+    it("should say Hello guys!", () => {
+      console.log("hello");
+    });
+  });
+});
