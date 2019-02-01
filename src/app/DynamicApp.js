cbd8b9eb07edbd4ff6120e875c7d0058e9da7be1
Marielle Pointon <marielle@widged.com>
2019-02-01 13:56:09 +0100

    quick fix for menu

--- src/app/DynamicApp.js
+++ src/app/DynamicApp.js
@@ -1,6 +1,6 @@
 // view
 import React, { Component } from "react";
-import { Row, Col } from "react-bootstrap";
+import { Grid, Row, Col } from "react-bootstrap";
 import {
   AppHeader,
   AppFooter
@@ -33,15 +33,17 @@ class DynamicApp extends Component {
       <div className="App">
         <header className="App-header">
           <AppHeader>
-            <Row>
-              <Col sm={6} />
-              <Col xs={6} sm={4}>
-                {UserProfile && <UserProfile />}
-              </Col>
-              <Col xs={6} sm={2}>
-                <ConnectedAppMenu />
-              </Col>
-            </Row>
+            <Grid>
+              <Row>
+                <Col md={4} xs={2} sm={2} />
+                <Col md={2} xs={6} sm={4}>
+                  {UserProfile && <UserProfile />}
+                </Col>
+                <Col md={2} xs={6} sm={2}>
+                  <ConnectedAppMenu />
+                </Col>
+              </Row>
+            </Grid>
           </AppHeader>
         </header>
         <ConnectedSettings />
