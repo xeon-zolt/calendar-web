b0bffebe37649eb6e21b5a51176887e1f9ae8685
Marielle Pointon <marielle@widged.com>
2019-02-04 16:07:02 +0100

    #30 moving settings to in-page display instead of modal

--- src/components/settings/Settings.js
+++ src/components/settings/Settings.js
@@ -3,6 +3,37 @@ import { Modal, Button } from "react-bootstrap";
 import Calendars from "./Calendars";
 import Contacts from "./Contacts";
 
+class SettingsPanel extends Component {
+  render() {
+    const { show, handleHide, CalendarsContent, ContactsContent } = this.props;
+    return (
+      <Modal show={show} onHide={handleHide}>
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title">Settings</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{CalendarsContent}</Modal.Body>
+        <Modal.Body>{ContactsContent}</Modal.Body>
+        <Modal.Footer>
+          <Button onClick={handleHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+}
+
+class SettingsPage extends Component {
+  render() {
+    const { show, handleHide, CalendarsContent, ContactsContent } = this.props;
+    return (
+      <div className="text-left" style={{ marginLeft: 100 }}>
+        <h4>Settings</h4>
+        {CalendarsContent}
+        {ContactsContent}
+      </div>
+    );
+  }
+}
+
 export default class Settings extends Component {
   render() {
     const {
@@ -17,33 +48,35 @@ export default class Settings extends Component {
       addContact,
       deleteContacts
     } = this.props;
+    const CalendarsContent = (
+      <div>
+        <label>Calendars</label>
+        <Calendars
+          calendars={calendars}
+          addCalendar={addCalendar}
+          deleteCalendars={deleteCalendars}
+          addCalendarUrl={addCalendarUrl}
+        />
+      </div>
+    );
+
+    const ContactsContent = (
+      <div>
+        <label>Contacts</label>
+        <Contacts
+          contacts={contacts}
+          lookupContacts={lookupContacts}
+          addContact={addContact}
+          deleteContacts={deleteContacts}
+        />
+      </div>
+    );
+
     return (
-      <Modal show={show} onHide={handleHide}>
-        <Modal.Header closeButton>
-          <Modal.Title id="contained-modal-title">Settings</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <label>Calendars</label>
-          <Calendars
-            calendars={calendars}
-            addCalendar={addCalendar}
-            deleteCalendars={deleteCalendars}
-            addCalendarUrl={addCalendarUrl}
-          />
-        </Modal.Body>
-        <Modal.Body>
-          <label>Contacts</label>
-          <Contacts
-            contacts={contacts}
-            lookupContacts={lookupContacts}
-            addContact={addContact}
-            deleteContacts={deleteContacts}
-          />
-        </Modal.Body>
-        <Modal.Footer>
-          <Button onClick={handleHide}>Close</Button>
-        </Modal.Footer>
-      </Modal>
+      <SettingsPage
+        CalendarsContent={CalendarsContent}
+        ContactsContent={ContactsContent}
+      />
     );
   }
 }
